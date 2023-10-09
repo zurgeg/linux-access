dace854eb1168cfde10c8e140bb3d507dc30f19c
zurgeg <46549042+zurgeg@users.noreply.github.com>
2023-10-09 15:41:18 -0400

    add native-access:// protocol

--- nafix.sh
+++ nafix.sh
@@ -2,3 +2,10 @@ sudo npm install -g asar
 asar extract "$WINEPREFIX/drive_c/Program Files/Native Instruments/Native Access/resources/app.asar" /tmp/nafix/extracted
 cp sudoprompt.js "@vscode/sudo-prompt/index.js"
 asar pack /tmp/nafix/extracted "$WINEPREFIX/drive_c/Program Files/Native Instruments/Native Access/resources/app.asar"
+touch ~/.local/share/applications/na-workaround.desktop
+echo "[Desktop Entry]" >> ~/.local/share/applications/na-workaround.desktop
+echo "Name=Native Access" >> ~/.local/share/applications/na-workaround.desktop
+echo "Exec=WINEPREFIX=$WINEPREFIX wine \"%ProgramFiles%/Native Instruments/Native Access\" \"%u\"" >> ~/.local/share/applications/na-workaround.desktop
+echo "Type=Application" >> ~/.local/share/applications/na-workaround.desktop
+echo "Terminal=false" >> ~/.local/share/applications/na-workaround.desktop
+echo "MimeType=x-scheme-handler/native-access;" >> ~/.local/share/applications/na-workaround.desktop
